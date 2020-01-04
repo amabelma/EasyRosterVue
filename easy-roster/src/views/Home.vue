ebca0e3cd343a7ec381028e84d1adb502c4b0f50
unknown <amabelma@mountunion.edu>
2020-01-03 23:55:03 -0500

    Update navigation and naming consistency info.

--- easy-roster/src/views/Home.vue
+++ easy-roster/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Welcome to EasyRoster!</h1>
+    <NavigationTable></NavigationTable>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavigationTable from '@/components/navigation-table.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    NavigationTable
   }
 }
 </script>
