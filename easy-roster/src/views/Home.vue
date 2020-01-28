62e19a14ddd268f47f5e33f59c61f158821a52a0
unknown <amabelma@mountunion.edu>
2020-01-27 19:34:57 -0500

    Updated main layout with additional setup

--- easy-roster/src/views/Home.vue
+++ easy-roster/src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
     <h1>Welcome to EasyRoster!</h1>
-    <NavigationTable></NavigationTable>
   </div>
+  
 </template>
 
 <script>
-import NavigationTable from '@/components/NavigationTable.vue'
 
 export default {
   name: 'home',
   components: {
-    NavigationTable
   }
 }
 </script>
